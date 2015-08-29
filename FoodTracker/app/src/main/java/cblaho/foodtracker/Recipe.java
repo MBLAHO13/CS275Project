d36d7bcb2a20f17259896a9fedda163005adcdd0
Max Mattes <mm3888@drexel.edu>
2015-08-29 12:27:37 -0400

    Finished Food interface, Ingredient Class and Recipe class

--- FoodTracker/app/src/main/java/cblaho/foodtracker/Recipe.java
+++ FoodTracker/app/src/main/java/cblaho/foodtracker/Recipe.java
@@ -1,11 +1,76 @@
 package cblaho.foodtracker;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * Created by cblaho on 8/26/15.
  */
-public class Recipe {
-    Map<Ingredient, Double> materials;
-    Map<Integer, String> steps;
+public class Recipe implements Food {
+    String id;
+    String name;
+    Double qty;
+    String conversion;
+    String steps;
+    List<Ingredient> ingredients;
+    Map<String,Double> conversions;
+
+    public Recipe(String id, String name, Double qty, List<Ingredient> ingredients, Map<String,Double> conversions, String conversion, String steps) {
+        this.id = id;
+        this.qty = qty;
+        this.name = name;
+        this.steps = steps;
+        this.conversions = conversions;
+        this.conversion = conversion;
+        this.ingredients = ingredients;
+    }
+
+    @Override
+    public String getID() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Double getNutrient(String name) {
+        Double nutrient = 0.0;
+        for(Ingredient i : ingredients) {
+            nutrient += i.getNutrient(name);
+        }
+        return nutrient*qty;
+    }
+
+    @Override
+    public Double getQty() {
+        return qty;
+    }
+
+    @Override
+    public void setQty(Double qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public Map<String, Double> getConversions() {
+        return conversions;
+    }
+
+    @Override
+    public String getConversion() {
+        return conversion;
+    }
+
+    @Override
+    public void setConversion(String name) {
+        conversion = name;
+    }
+
+    @Override
+    public void addConversion(String name, Double grams) {
+        conversions.put(name, grams);
+    }
 }
