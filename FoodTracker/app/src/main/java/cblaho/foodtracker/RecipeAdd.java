9b652532a995ea92ae3c25c86b975e2dbaef9a83
Colleen blaho <cpb49@drexel.edu>
2015-09-02 13:56:05 -0400

    i did more things

--- FoodTracker/app/src/main/java/cblaho/foodtracker/RecipeAdd.java
+++ FoodTracker/app/src/main/java/cblaho/foodtracker/RecipeAdd.java
@@ -1,18 +1,45 @@
 package cblaho.foodtracker;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class RecipeAdd extends Activity {
+import java.util.Map;
+
+public class RecipeAdd extends Activity implements CacheListener {
     Recipe data;
+    private Cache cache;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.addingredient);
-            Intent intent = getIntent();
-            //data = intent.getParcelableExtra(MainActivity.ADDRESS_DATA);
-            //TextView title = (TextView) findViewById(R.id.titleText);
+        super.onCreate(savedInstanceState);
+        this.cache = new Cache(this, getApplicationContext());
+        setContentView(R.layout.addingredient);
+        ActionBar ab = getActionBar();
+        ab.setTitle("Add Ingredients");
+        //todo: sort out the steps
+        ab.setSubtitle("Step One of Two");
+        Intent intent = getIntent();
+        //data = intent.getParcelableExtra(MainActivity.ADDRESS_DATA);
+        //TextView title = (TextView) findViewById(R.id.titleText);
+    }
+
+    public void searchIngredient(){
+        EditText mEdit   = (EditText)findViewById(R.id.recipe_add_searchbox);
+        String query = mEdit.getText().toString();
+        cache.searchFood(query);
+
+    }
+
+    @Override
+    public void onFoodFound(Food f) {
+        //pass information to add unit/quantity activity
+    }
+
+    @Override
+    public void onSearchResult(Map<String, String> results) {
+        //pass information to the list view thing
     }
 }
