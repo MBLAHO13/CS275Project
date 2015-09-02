a2384cb2296fec14d6a281aa977e0c58d31b6347
Colleen blaho <cpb49@drexel.edu>
2015-09-02 17:35:09 -0400

    adapter for addingredient search results

--- FoodTracker/app/src/main/java/cblaho/foodtracker/SearchResults.java
+++ FoodTracker/app/src/main/java/cblaho/foodtracker/SearchResults.java
@@ -1,13 +1,52 @@
 package cblaho.foodtracker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SearchResults extends Activity {
-
+    private Cache cache;
+    private Recipe recipe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+        Intent i = getIntent();
+        Recipe recipe = i.getParcelableExtra("recipe");
+        Map<String, String> results = (HashMap) i.getSerializableExtra("results");
+        ArrayAdapter arrayAdapter;
+        ListView listView;
+        listView = (ListView) findViewById(R.id.recipeList);
+        if(!results.isEmpty()) {
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> a, View v, int position,
+                                        long id) {
+                    Intent intent = new Intent(SearchResults.this, RecipeSteps.class);
+                    RecipePair entry = (RecipePair) a.getItemAtPosition(position);
+                    intent.putExtra("ingredient", cache.getIngredientById(entry.getId()));
+                    startActivity(intent);
+                }
+            });
+
+            ArrayList<RecipePair> SearchPairs = new ArrayList<>();
+            for (Map.Entry<String, String> entry : results.entrySet()) {
+                SearchPairs.add(new RecipePair(entry.getKey(), entry.getValue()));
+            }
+            RecipePair[] packedPairs = SearchPairs.toArray(new RecipePair[SearchPairs.size()]);
+            arrayAdapter = new SearchResultsAdapter(this, R.layout.search_results_row, packedPairs);
+        } else {
+            //this shit better not happen
+            arrayAdapter = new ArrayAdapter(this,R.layout.search_results_row, R.id.search_result_name, new String[] {"No Results Found"});
+        }
+        listView.setAdapter(arrayAdapter);
     }
 }
