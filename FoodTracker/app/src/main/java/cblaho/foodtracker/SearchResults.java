f73436ae2b95e4cfd11b0de0a870792917e6b196
Colleen blaho <cpb49@drexel.edu>
2015-09-02 16:58:30 -0400

    APPCOMPATACTIVITY MUST DIEEEEEE

--- FoodTracker/app/src/main/java/cblaho/foodtracker/SearchResults.java
+++ FoodTracker/app/src/main/java/cblaho/foodtracker/SearchResults.java
@@ -1,9 +1,9 @@
 package cblaho.foodtracker;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class SearchResults extends AppCompatActivity {
+public class SearchResults extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
