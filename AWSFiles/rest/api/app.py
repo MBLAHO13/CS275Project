8e63760b52373490ab5d8e9b42dd2e31bca9e0d4
EC2 Default User <ec2-user@ip-172-31-26-124.us-west-2.compute.internal>
2015-09-03 02:30:48 +0000

    Added updates for the working rest api

--- AWSFiles/rest/api/app.py
+++ AWSFiles/rest/api/app.py
@@ -17,7 +17,7 @@ def getIdResults(ingredient_id):
    conn.row_factory = dict_factory
    c = conn.cursor()
    query = 'SELECT * from ingredients where id=(?);'
-   c.execute(query, (ingredient_id,))
+   c.execute(query, (int(ingredient_id),))
    query_result = c.fetchone()
    #if database doesn't have the id
    print(str(ingredient_id).zfill(5))
@@ -53,30 +53,59 @@ def getIdResults(ingredient_id):
    return response
 
 def getNameResults(ingredient_name):
-   url = "http://api.nal.usda.gov/ndb/search/?format=json&q="+ingredient_name+"&sort=n&max=100&offset=0&api_key=xRqfBLIxnQHgOZQJkJDHhBR2X95Vlhs002Y58Lfr"
-   url_results = requests.get(url).json()
-   response = {
+   def dict_factory(cursor, row):
+      d = {}
+      for idx, col in enumerate(cursor.description):
+         d[col[0]] = row[idx]
+      return d
+
+   #First checking in databse
+   conn = sqlite3.connect('../../ingredients.db', detect_types=sqlite3.PARSE_COLNAMES)
+   conn.row_factory = dict_factory
+   c = conn.cursor()
+   query = 'SELECT * from ingredients where name=(?);'
+   c.execute(query, (str(ingredient_name),))
+   query_result = c.fetchone()
+   #if database doesn't have the name
+   if query_result is None: 
+      url = "http://api.nal.usda.gov/ndb/search/?format=json&q="+ingredient_name+"&sort=n&max=100&offset=0&api_key=xRqfBLIxnQHgOZQJkJDHhBR2X95Vlhs002Y58Lfr"
+      url_results = requests.get(url).json()
+      response = {
               'response': 'search',
               'result':[] }
-   for ingredient in url_results['list']['item']:
-      response['result'].append({'name': ingredient['name'], 'id': ingredient['ndbno']})
+      for ingredient in url_results['list']['item']:
+         response['result'].append({'name': ingredient['name'], 'id': ingredient['ndbno']})
+      return response
+   
+   response = {
+    'response': 'ingredient',
+    'id': query_result['id'],
+    'name': query_result['name'],
+    'group_name': query_result['group_name'],
+    'nutrients': []
+   }
+   for key in query_result.keys():
+      if key is not ('name' or 'id' or 'group_name'):
+         if query_result[key] is not None:
+            response['nutrients'].append({'name':key,'value':query_result[key]})
    return response
 
 @app.route('/', methods=['GET'])
 def index():
-    response = "Hello, use 'http://currentIP/rest/api/ingredient/input_id' to get the ingredient info \nuse 'http://currentIP/rest/api/search/input_string' to get search results"
+    response = "Hello, World"
     return response
 
 @app.route('/rest/api/id/', methods=['GET'])
 def ingredient():
+   ingredient_id = request.headers.get('request-value')
    response = getIdResults(ingredient_id)
    return jsonify(response)
 
 @app.route('/rest/api/name/', methods=['GET'])
 def search():
-   ingredient_name = request.headers.get('name')
+   ingredient_name = request.headers.get('request-value')
    response = getNameResults(ingredient_name)
    return jsonify(response)
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0')
-    print getIdResults(1001)
+    app.run(host='0.0.0.0')
+    #print getNameResults("Cheese")
